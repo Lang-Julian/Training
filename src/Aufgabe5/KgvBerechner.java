d810a70a534dc3cac8b58bc0784bbc9a276bca5c
Julian Lang <120318573+Lang-Julian@users.noreply.github.com>
2023-03-09 15:41:41 +0100

    Aufgabe 5 Finished

--- src/Aufgabe5/KgvBerechner.java
+++ src/Aufgabe5/KgvBerechner.java
@@ -6,29 +6,25 @@ public class KgvBerechner {
 
     public static void main(String[] args) {
 
-        double kgv;
-
         Scanner sc = new Scanner(System.in);
         System.out.print("Zahl 1: ");
-        double zahl1 = sc.nextDouble();
+        int zahl1 = sc.nextInt();
         System.out.print("Zahl 2: ");
-        double zahl2 = sc.nextDouble();
+        int zahl2 = sc.nextInt();
         sc.close(); // close the Scanner object
 
-        kgv = berechneKgv(zahl1, zahl2);
+        int kgv = berechneKgv(zahl1, zahl2);
 
         System.out.println("Das kleinste gemeinsame Vielfache von " + zahl1 + " und " + zahl2 + " ist: " + kgv);
 
     }
 
-    public static double berechneKgv(double zahl1, double zahl2) {
-        double kgv;
-        double max = Math.max(zahl1, zahl2);
+    public static int berechneKgv(int zahl1, int zahl2) {
+        int max = Math.max(zahl1, zahl2);
         int iterations = 0;
         while (iterations < 1000000) {
             if (max % zahl1 == 0 && max % zahl2 == 0) {
-                kgv = max;
-                return kgv;
+                return max;
             }
             max++;
             iterations++;
--- test/Aufgabe5/KgvBerechnerTest.java
+++ test/Aufgabe5/KgvBerechnerTest.java
@@ -7,16 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class KgvBerechnerTest {
 
     @Test
-    public void main() {
+    public void testKgvBerechner() {
         // Arrange
-        double zahl1 = 15;
-        double zahl2 = 25;
-        double expectedKgv = 75;
+        int zahl1 = 15;
+        int zahl2 = 25;
+        int expectedKgv = 75;
 
         // Act
-        double resultKgv = KgvBerechner.berechneKgv(zahl1, zahl2);
+        int resultKgv = KgvBerechner.berechneKgv(zahl1, zahl2);
 
         // Assert
-        assertEquals(expectedKgv, resultKgv, 0.0);
+        assertEquals(expectedKgv, resultKgv);
+
+        // Arrange
+        zahl1 = 18;
+        zahl2 = 24;
+        expectedKgv = 72;
+
+        // Act
+        resultKgv = KgvBerechner.berechneKgv(zahl1, zahl2);
+
+        // Assert
+        assertEquals(expectedKgv, resultKgv);
     }
 }
