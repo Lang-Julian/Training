4cc33cf4d4ccb24fa7bf316e3bb03774c4ad0be0
Julian Lang <120318573+Lang-Julian@users.noreply.github.com>
2023-03-14 14:02:10 +0100

    Aufgabe 13 Complete

--- src/Aufgabe13/Person.java
+++ src/Aufgabe13/Person.java
@@ -1,48 +1,50 @@
 package Aufgabe13;
 
 public class Person {
-    private int alter;
-    private String vorname;
-    private String nachname;
-    private int groesse;
-    private int gewicht;
+    private int alter; // Attribut für das Alter
+    private String vorname; // Attribut für den Vornamen
+    private String nachname; // Attribut für den Nachnamen
+    private int groesse; // Attribut für die Größe
+    private int gewicht; // Attribut für das Gewicht
 
     public Person(int alter, String vorname, String nachname, int groesse, int gewicht) {
-        this.alter = alter;
-        this.vorname = vorname;
-        this.nachname = nachname;
-        this.groesse = groesse;
-        this.gewicht = gewicht;
+        this.alter = alter; // Initialisierung des Attributs Alter
+        this.vorname = vorname; // Initialisierung des Attributs Vorname
+        this.nachname = nachname; // Initialisierung des Attributs Nachname
+        this.groesse = groesse; // Initialisierung des Attributs Größe
+        this.gewicht = gewicht; // Initialisierung des Attributs Gewicht
     }
 
     public int getAlter() {
-        return alter;
+        return alter; // Getter-Methode für das Attribut Alter
     }
 
     public String getVorname() {
-        return vorname;
+        return vorname; // Getter-Methode für das Attribut Vorname
     }
 
     public String getNachname() {
-        return nachname;
+        return nachname; // Getter-Methode für das Attribut Nachname
     }
 
     public int getGroesse() {
-        return groesse;
+        return groesse; // Getter-Methode für das Attribut Größe
     }
 
     public int getGewicht() {
-        return gewicht;
+        return gewicht; // Getter-Methode für das Attribut Gewicht
     }
 
+    // Überladene equals-Methode, die prüft, ob zwei Personenobjekte gleich sind
     public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
+        if (obj == this) { // Prüfen, ob das übergebene Objekt dieselbe Referenz hat wie das aktuelle Objekt
+            return true; // Wenn ja, sind sie gleich
         }
-        if (!(obj instanceof Person)) {
-            return false;
+        if (!(obj instanceof Person)) { // Prüfen, ob das übergebene Objekt vom Typ Person ist
+            return false; // Wenn nein, sind sie nicht gleich
         }
-        Person other = (Person) obj;
+        Person other = (Person) obj; // Casten des übergebenen Objekts zu einem Person-Objekt
+        // Prüfen, ob alle Attribute des aktuellen Objekts und des übergebenen Objekts übereinstimmen
         return this.alter == other.alter &&
                 this.vorname.equals(other.vorname) &&
                 this.nachname.equals(other.nachname) &&
--- test/Aufgabe13/PersonTest.java
+++ test/Aufgabe13/PersonTest.java
@@ -3,16 +3,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PersonTest {
+
+    // Hier testen wir die equals() Methode der Person-Klasse.
+
     @Test
     public void testEquals() {
+
+        // Wir erstellen drei Personen-Objekte mit unterschiedlichen Attributen.
+
         Person p1 = new Person(25, "Max", "Mustermann", 180, 75);
         Person p2 = new Person(25, "Max", "Mustermann", 180, 75);
         Person p3 = new Person(30, "Anna", "Musterfrau", 170, 60);
 
-        // Positiver Testfall
+        // Hier testen wir einen positiven Fall, in dem p1 und p2 denselben Attribut-Wert haben.
+
         assertEquals(true, p1.equals(p2));
 
-        // Negativer Testfall
+        // Hier testen wir einen negativen Fall, in dem p1 und p3 unterschiedliche Attribut-Werte haben.
+
         assertEquals(false, p1.equals(p3));
     }
 }
