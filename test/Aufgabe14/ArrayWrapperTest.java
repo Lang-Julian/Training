79b68a16a1052d23e78875b1c6f5879222e400e8
Julian Lang <120318573+Lang-Julian@users.noreply.github.com>
2023-03-15 10:47:38 +0100

    Aufgabe 14 Complete

--- test/Aufgabe14/ArrayWrapperTest.java
+++ test/Aufgabe14/ArrayWrapperTest.java
@@ -13,15 +13,10 @@ class ArrayWrapperTest {
         aw.add(3);
         aw.add(4);
         assertEquals(4, aw.getElementCount());
-    }
-    @Test
-    public void testGet() {
-        ArrayWrapper aw = new ArrayWrapper(3);
-        aw.add(1);
-        aw.add(2);
-        aw.add(3);
-        aw.add(4);
+        assertEquals(1, aw.get(0));
         assertEquals(2, aw.get(1));
+        assertEquals(3, aw.get(2));
+        assertEquals(4, aw.get(3));
     }
     @Test
     public void testGetElementCount() {
